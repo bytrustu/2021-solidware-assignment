e914c741019bded6dd168558eabd334a86c1978b
bytrustu <bytrustu@gmail.com>
2021-02-01 05:02:51 +0900

    [CLIENT/FIX] 클라이언트 포트변경

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
-  LOCAL_URL: 'http://localhost:9000',
-  SERVER_URL: 'http://localhost:9000'
+  LOCAL_URL: "http://localhost:9005",
+  SERVER_URL: "http://133.186.159.157:9005",
 };
 
 export default config;
